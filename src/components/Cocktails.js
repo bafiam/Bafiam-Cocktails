7c94c71971a8fcc3e077bd2359270cac5fb05045
bafiam <stevebafiam@gmail.com>
2020-06-30 16:55:59 +0300

    feature:styling:bootstrap on cocktails

--- src/components/Cocktails.js
+++ src/components/Cocktails.js
@@ -1,41 +1,40 @@
-
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 export class Cocktails extends Component {
   render() {
-    const {cocktails} = this.props
-    let mapData
-    if (cocktails.length === 11){
-      mapData = cocktails.map((cocktail) =>{
-        return(
-          cocktail[1].map((res) => {
-            return (
-              <div>
-              <p>{res.strDrink}</p>
-              <img src={res.strDrinkThumb} alt="..." class="img-thumbnail "></img>
-            
+    const { cocktails } = this.props;
+    let mapData;
+    if (cocktails.length === 11) {
+      mapData = cocktails.map((cocktail) => {
+        return cocktail[1].map((res) => {
+          return (
+            <div className="card-group w-25 h-25">
+              <div className="card">
+                <img
+                  className="card-img-top w-100 h-100;"
+                  src={res.strDrinkThumb}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{res.strDrink}</h5>
+                </div>
               </div>
-              )
-          })
-          )
-      })
-    }else{
-      mapData = <p>Loading...</p>
+            </div>
+          );
+        });
+      });
+    } else {
+      mapData = <div class="alert alert-secondary" role="alert">
+      Loading...
+    </div>
     }
-    return (
-      <div>
-        {mapData}
-      </div>
-    )
+    return <div className='container d-flex align-content-around flex-wrap'>{mapData}</div>;
   }
 }
 
 const mapStateToProps = (state) => ({
-  cocktails: state.cocktails
-  
-})
+  cocktails: state.cocktails,
+});
 
-
-
-export default connect(mapStateToProps, null)(Cocktails)
+export default connect(mapStateToProps, null)(Cocktails);
