ef55da9770f9dd0daae7f4ed9bc55ce166490b56
bafiam <stevebafiam@gmail.com>
2020-07-04 01:34:47 +0300

    test: add test cases for the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => (
   <nav className="navbar sticky-top navbar-dark bg-primary mb-3">
     <div className="container">
 
-      <Link to="/" className="navbar-brand btn btn-link">Black Widow CockTails</Link>
+      <Link to="/" className="navbar-brand btn btn-link" data-testid="navbar-brand">Black Widow CockTails</Link>
 
     </div>
   </nav>
--- src/components/__test__/Navbar.test.js
+++ src/components/__test__/Navbar.test.js
@@ -1,2 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import Navbar from "../Navbar";
+import { render } from "@testing-library/react";
 
-
+describe('Test the Navbar', () => {
+  test('should have a / url in the href attribute', () => {
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Navbar></Navbar>
+      </BrowserRouter>
+    );
+    expect(getByTestId("navbar-brand")).toHaveAttribute("href", "/");
+    
+    
+  })
+  
+  
+})
