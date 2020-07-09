791f8f306bbce48716cfffa7d96a98143253b1eb
bafiam <stevebafiam@gmail.com>
2020-07-09 15:27:59 +0300

    feature: add card text and card footer responsive

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const CocktailCard = props => {
+const CocktailCard = (props) => {
   const { response } = props;
 
   return (
-    <div className="card-group  mb-2">
-      <div className="card ">
-        <img
-          className="card-img-top"
-          data-testid="card-img-top"
-          src={response.strDrinkThumb}
-          alt="Card"
-        />
-        <div className="card-body text-center">
-          <h5 className="card-title " data-testid="card-title">
-            {response.strDrink}
-          </h5>
-        </div>
-        <div className="card-footer mr-auto ml-auto">
-          <Link
-            to={`/cocktail/${response.idDrink}`}
-            className="btn btn-info"
-            data-testid="card-btn"
-          >
-            {' '}
-            Instructions
-          </Link>
-        </div>
+    <div className="card ">
+      <img
+        className="card-img-top"
+        data-testid="card-img-top"
+        src={response.strDrinkThumb}
+        alt="Card"
+      />
+      <div className="card-body text-center">
+        <h5 className="card-title " data-testid="card-title">
+          {response.strDrink}
+        </h5>
+      </div>
+      <div className="card-footer">
+        <Link
+          to={`/cocktail/${response.idDrink}`}
+          className="btn btn-info m-auto"
+          data-testid="card-btn"
+        >
+          {" "}
+          Instructions
+        </Link>
       </div>
     </div>
   );
@@ -43,9 +41,9 @@ CocktailCard.propTypes = {
 };
 CocktailCard.defaultProps = {
   response: PropTypes.shape({
-    strDrink: '',
-    idDrink: '',
-    strDrinkThumb: '',
+    strDrink: "",
+    idDrink: "",
+    strDrinkThumb: "",
   }),
 };
 export default CocktailCard;
